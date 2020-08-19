6dd0cc08df66cb71795ebf152aeb4fde75e2920e
Jak <jackji2016@hotmail.com>
2020-08-19 14:15:47 -0700

    move project to git

---
+++ TheDeepDungeon/Assets/Script/MouseAim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAim : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+    private void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        transform.position = new Vector2(mousePosition.x, mousePosition.y);
+    }
+}
---
+++ TheDeepDungeon/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidbody2D;
+    public Animator animator;
+
+    private Vector2 movement;
+    private Vector2 lastMovement = new Vector2(1f, 0f);
+
+    internal static class MoveStates
+    {
+        internal const string Horizontal = "Horizontal";
+        internal const string Vertical = "Vertical";
+        internal const string Speed = "Speed";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        StoreLastMovement();
+        Animate();
+        FlipPlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        //rigidbody2D.MovePosition(rigidbody2D.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rigidbody2D.velocity = new Vector2(movement.x, movement.y) * moveSpeed;
+    }
+
+    private void ProcessInput()
+    {
+        movement.x = Input.GetAxisRaw(MoveStates.Horizontal);
+        movement.y = Input.GetAxisRaw(MoveStates.Vertical);
+    }
+
+    private void StoreLastMovement()
+    {
+        if (Mathf.Abs(movement.x) > Mathf.Epsilon || Mathf.Abs(movement.y) > Mathf.Epsilon)
+        {
+            lastMovement.x = movement.x;
+            lastMovement.y = movement.y;
+        }
+    }
+
+    private void Animate()
+    {
+        animator.SetFloat(MoveStates.Horizontal, lastMovement.x);
+        animator.SetFloat(MoveStates.Vertical, lastMovement.y);
+        animator.SetFloat(MoveStates.Speed, movement.sqrMagnitude);
+    }
+
+    private void FlipPlayer()
+    {
+        var playerHasHorizontalSpeed = Mathf.Abs(lastMovement.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed) transform.localScale = new Vector2(lastMovement.x, 1);
+    }
+}
